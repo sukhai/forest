2d6ceaedc1d5519d6a796a6a945594ef14aaf562
Su Khai Koh <8684653+sukhai@users.noreply.github.com>
2020-06-17 20:08:12 -0700

    Create a deploy docs script

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT=$(git rev-parse --show-toplevel)
+
+cd "$ROOT"
+
+temp_dir=$(mktemp -d 2>/dev/null || mktemp -d -t 'temp_dir')
+trap "{ rm -rf ${temp_dir}; }" EXIT
+
+# Generate all the docs
+./gradlew :docs:orchidDeploy -PorchidEnvironment=prod
+
+mv docs/build/docs/orchid/* "$temp_dir"
+
+git stash
+git checkout gh-pages
+
+mv "$temp_dir/*" .
+
+git add .
+git commit -m "Update documentation."
+git push origin
