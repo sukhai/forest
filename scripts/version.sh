815ffc4df0d8154510d72af8ae8ba803956d761c
Su Khai Koh <8684653+sukhai@users.noreply.github.com>
2020-06-19 15:52:59 -0700

    Update release.yml to create release with changelog body

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,6 +4,17 @@ ROOT=$(git rev-parse --show-toplevel)
 PROJECT_VERSION_FILE="${ROOT}/gradle/release.properties"
 CHANGELOG_DIR="${ROOT}/docs/src/orchid/resources/changelog"
 
+function _get_latest_changelog_content() {
+    local latest_changelog_version
+
+    latest_changelog_version=$(find "$CHANGELOG_DIR" -type f -regex ".*\.md" \
+        | sed 's/.*\/\(.*\)\.md/\1/' \
+        | sort \
+        | tail -n 1)
+
+    cat "${CHANGELOG_DIR}/${latest_changelog_version}.md"
+}
+
 function _get_latest_changelog_version() {
     local latest_changelog_version
 
@@ -41,6 +52,11 @@ function update_properties() {
     output=$(sed "s/release\.version=.*$/release\.version=$latest_changelog_version/g" "$release_file")
     echo -n "$output" > "$release_file"
 }
+
+if [[ "$1" == 'get_latest_changelog_content' ]]; then
+    _get_latest_changelog_content
+    exit 0
+fi
 
 if [[ "$(_has_new_changelog)" == 'true' ]]; then
     _get_latest_changelog_version
