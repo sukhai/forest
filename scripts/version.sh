a496aaeb081f6e271a32f97b3ef2a59769ba44be
Su Khai Koh <8684653+sukhai@users.noreply.github.com>
2020-06-19 20:40:28 -0700

    Update GitHub actions to create changelog on webpage (#13)
    * Test github actions variables
    
    * Update github action trigger event to test workflow
    
    * Update github action to debug trigger event
    
    * Revert ci.yml because we will create a new workflow in different file
    
    * Create release.yml to have release build logic
    
    * Create version.sh to handle project versioning
    
    * Create a 1.2.1 version changelog to test new github action workflow
    
    * Fix release.yml syntax
    
    * Update and test release.yml
    
    * Update release.yml to create release with changelog body
    
    * Fix release.yml not reading all changelog content
    
    * Attemp to fix not all lines of changelog content get set to release body
    
    * Attemp again to fix not all lines of changelog content get set to release body
    
    * Attemp again to fix not all lines of changelog content get set to release body
    
    * Update version.sh to accept arguments
    
    * Update release.yml to update project version and deploy docs when a new Release is published
    
    * Remove debugging code in version.sh
    
    * Fix release.yml not getting triggered when a Release is published
    
    * Update to version
    
    * Fix release.yml take in wrong argument for new release version
    
    * Update to version 1.2.1
    
    * Remove test changelog file 1.2.1.md
    
    * Remove test code in release.yml
    
    * Revert debugging code

---
+++ scripts/version.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+ROOT=$(git rev-parse --show-toplevel)
+PROJECT_VERSION_FILE="${ROOT}/gradle/release.properties"
+CHANGELOG_DIR="${ROOT}/docs/src/orchid/resources/changelog"
+
+function _get_latest_changelog_content() {
+    local latest_changelog_version
+
+    latest_changelog_version=$(find "$CHANGELOG_DIR" -type f -regex ".*\.md" \
+        | sed 's/.*\/\(.*\)\.md/\1/' \
+        | sort \
+        | tail -n 1)
+
+    cat "${CHANGELOG_DIR}/${latest_changelog_version}.md"
+}
+
+function _get_latest_changelog_version() {
+    local latest_changelog_version
+
+    latest_changelog_version=$(find "$CHANGELOG_DIR" -type f -regex ".*\.md" \
+        | sed 's/.*\/\(.*\)\.md/\1/' \
+        | sort \
+        | tail -n 1)
+
+    echo "$latest_changelog_version"
+}
+
+function _has_new_changelog() {
+    local latest_changelog_version
+    local project_version
+
+    latest_changelog_version=$(_get_latest_changelog_version)
+    project_version=$(cat "$PROJECT_VERSION_FILE" \
+        | grep -e 'release.version' \
+        | sed 's/.*\=\(.*\)$/\1/')
+
+    if [[ "$latest_changelog_version" == "$project_version" ]]; then
+        echo false
+    else
+        echo true
+    fi
+}
+
+function _update_properties() {
+    new_version="$1"
+    echo "Updating to version $new_version"
+
+    local release_file="$ROOT/gradle/release.properties"
+    local output=""
+    output=$(sed "s/release\.version=.*$/release\.version=$new_version/g" "$release_file")
+    echo -n "$output" > "$release_file"
+}
+
+function main() {
+    local get_changelog=false
+    local update_project_version=false
+    local new_project_version
+
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -c | --changelog)
+                get_changelog=true
+                shift # past argument
+                ;;
+            -v | --project-version)
+                update_project_version=true
+                new_project_version="$2"
+                shift # past argument
+                shift # past value
+                ;;
+            *)
+                echo "Unknown parameter passed: $1"
+                exit 1
+                ;;
+        esac
+    done
+
+    if [[ "$get_changelog" == "true" ]] && [[ "$update_project_version" == "true" ]]; then
+        echo "Only one of the two options can be enabled at a time."
+        exit 1
+    fi
+
+    if [[ "$get_changelog" == "true" ]]; then
+        _get_latest_changelog_content
+        exit 0
+    fi
+
+    if [[ "$update_project_version" == "true" ]]; then
+        if [[ -z "$new_project_version" ]]; then
+            echo "Mising argument for -v|--project-version"
+            exit 1
+        fi
+        _update_properties "$new_project_version"
+        exit 0
+    fi
+
+    if [[ "$(_has_new_changelog)" == "true" ]]; then
+        _get_latest_changelog_version
+    else
+        echo "No new changelog"
+        echo ""
+    fi
+}
+
+main "$@"
